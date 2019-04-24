335f0965d7c5b1417f81b246d1fa032ed23516b9
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:45:02 -0400

    Add solution to "Functions in C".
    Added source code solution validated by "Testcase 0".  Add validation test
    case 0.

--- C/easy/FunctionsInC/FunctionsInC.c
+++ C/easy/FunctionsInC/FunctionsInC.c
@@ -1,10 +1,13 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
+#include <stdio.h>
 
 /*
 Add `int max_of_four(int a, int b, int c, int d)` here.
 */
+int max_of_four(int a, int b, int c, int d) {
+    int max1=(a>b)?a:b;
+    int max2=(c>d)?c:d;
+    return((max1>max2)?max1:max2);
+}
 
 int main() {
     int a, b, c, d;
