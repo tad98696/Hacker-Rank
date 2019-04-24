c91e932a267ca4945612bee54df786ec2c923c0f
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:17 -0400

    Add solution to "Java Stdin and Stdout II".
    Added source code solution validated by "Testcase 0" and "Testcase 1".  Add
    validation test case 0 and 1.

--- Java/easy/JavaStdinAndStdoutII/JavaStdinAndStdoutII.java
+++ Java/easy/JavaStdinAndStdoutII/JavaStdinAndStdoutII.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
-public class Solution {
+public class JavaStdinAndStdoutII {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
 
         // Write your code here.
+        double d = scan.nextDouble();
+        scan.nextLine(); // Throw away the remainder of the double line.
+        String s = scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
