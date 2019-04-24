3c02e900a6396a6eb14c4e93e64e70638f10ef78
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:26 -0400

    Add solution to "Java Datatypes".
    Added source code solution validated by "Testcase 0".  Add validation test
    case 0.

--- Java/easy/JavaDatatypes/JavaDatatypes.java
+++ Java/easy/JavaDatatypes/JavaDatatypes.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 
 
-class Solution{
+class JavaDatatypes{
     public static void main(String []argh)
     {
 
@@ -21,6 +21,9 @@ class Solution{
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
                 //Complete the code
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                if(x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
             }
             catch(Exception e)
             {
