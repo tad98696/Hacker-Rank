b544251e3e3fad25331e2049d6741cdf338177c5
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:45:19 -0400

    Add original code for "Conditional Statements".
    https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem

---
+++ C++/easy/ConditionalStatements/ConditionalStatements.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    // Write Your Code Here
+
+    return 0;
+}
