ca9cfc1293cd9970fce38cae4e99e96d6ea0622d
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:28 -0400

    Add solution to "Java End-of-File".
    Added source code solution validated by "Testcase 1".  Add validation test
    case 1.

--- Java/easy/JavaEndOfFile/JavaEndOfFile.java
+++ Java/easy/JavaEndOfFile/JavaEndOfFile.java
@@ -1,9 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class JavaEndOfFile {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner sc = new Scanner(System.in);
+      String line;
+      int i = 0;
+      while(sc.hasNext()) {
+        line = sc.nextLine();
+        System.out.printf("%d %s%n", ++i, line);
+      }
     }
 }
