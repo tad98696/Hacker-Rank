b5d4c90cd6b235bfdd64880d2b561a726cf16291
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:45:15 -0400

    Add solution to "Input and Output".
    Added source code solution validated by "Testcase 0". Add validation test
    case 0.

--- C++/easy/InputAndOutput/InputAndOutput.cpp
+++ C++/easy/InputAndOutput/InputAndOutput.cpp
@@ -8,5 +8,9 @@ using namespace std;
 
 int main() {
       /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+      int a, b, c;
+      cin >> a >> b >> c;
+      cout << a + b + c << endl;
+
       return 0;
 }
