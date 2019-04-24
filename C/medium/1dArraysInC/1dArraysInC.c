feb44550887225ac22b87e62c1ca8a6ac5e68e8e
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:45:10 -0400

    Add solution to "1D Arrays in C".
    Added source code solution validated by "Testcase 0" and "Testcase 1".  Add
    validation test case 0 and 1.

--- C/medium/1dArraysInC/1dArraysInC.c
+++ C/medium/1dArraysInC/1dArraysInC.c
@@ -6,5 +6,18 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, sum = 0;
+    int *arr;
+
+    scanf("%d", &n);
+    arr = (int*)malloc(n*sizeof(int));
+
+    for(int i = 0; i < n; ++i) {
+      scanf("%d", &arr[i]);
+      sum += arr[i];
+    }
+
+    printf("%d\n", sum);
+
     return 0;
 }
