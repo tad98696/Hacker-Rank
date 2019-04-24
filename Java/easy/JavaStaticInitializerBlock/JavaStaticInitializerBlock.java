6e4b44dc893fd417abbf12e2d57f3a88d448fda8
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:29 -0400

    Add original code for "Java Static Initializer Block".
    https://www.hackerrank.com/challenges/java-static-initializer-block/problem

---
+++ Java/easy/JavaStaticInitializerBlock/JavaStaticInitializerBlock.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+Write your code here
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+
+	}//end of main
+
+}//end of class
