37b9e50293d094db0a4cb20c05430612acc98c5f
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:24 -0400

    Add solution to "Java Loops II".
    Added source code solution validated by "Testcase 0" and "Testcase 1".  Add
    validation test case 0 and 1.

--- Java/easy/JavaLoopsII/JavaLoopsII.java
+++ Java/easy/JavaLoopsII/JavaLoopsII.java
@@ -1,14 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-class Solution{
+class JavaLoopsII{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int q=in.nextInt();
+        for(int i=0;i<q;i++){
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+
+            int y = a;
+            String[] output = new String[n];
+            for(int x = 0; x < n; ++x) {
+              y = y + (int)Math.round(Math.pow(2, x)*b);
+              output[x] = Integer.toString(y);
+            }
+            System.out.printf("%s%n", String.join(" ", output));
         }
         in.close();
     }
