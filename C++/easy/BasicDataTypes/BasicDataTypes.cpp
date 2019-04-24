75cd1f58986b470508c5650b1059d8902adf9d3e
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:45:17 -0400

    Add solution to "Basic Data Types".
    Add source code solution validated by "Testcase 0" and "Testcase 1".  Add
    validation test case 0 and 1.

--- C++/easy/BasicDataTypes/BasicDataTypes.cpp
+++ C++/easy/BasicDataTypes/BasicDataTypes.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <cstdio>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    cin >> i >> l >> c >> f >> d;
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << d << endl;
     return 0;
 }
