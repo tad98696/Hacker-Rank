68f881df20fbb5f358474413369283676b37c9c8
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-24 19:44:19 -0400

    Add solution to "Java Output Formatting".
    Added source code solution validated by "Testcase 0" and "Testcase 1".  Add
    validation test case 0 and 1.

--- Java/easy/JavaOutputFormatting/JavaOutputFormatting.java
+++ Java/easy/JavaOutputFormatting/JavaOutputFormatting.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class JavaOutputFormatting {
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
@@ -10,6 +10,7 @@ public class Solution {
                 String s1=sc.next();
                 int x=sc.nextInt();
                 //Complete this line
+                System.out.printf("%-15s%03d%n", s1, x);
             }
             System.out.println("================================");
 
