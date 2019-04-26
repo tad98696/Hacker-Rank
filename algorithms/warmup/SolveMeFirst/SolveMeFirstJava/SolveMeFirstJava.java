a5ac2acb8854fadf9e8dd651d588ea0cdfac4fd9
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-26 18:52:45 -0400

    Add Java solution for "Solve Me First".
    Add source code solution validated by "Testcase 0" and "Testcase 1".

--- algorithms/warmup/SolveMeFirst/SolveMeFirstJava/SolveMeFirstJava.java
+++ algorithms/warmup/SolveMeFirst/SolveMeFirstJava/SolveMeFirstJava.java
@@ -4,22 +4,22 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SolveMeFirstJava {
 
 
-	static int solveMeFirst(int a, int b) {
-		// Hint: Type return a+b; below
+  static int solveMeFirst(int a, int b) {
+    // Hint: Type return a+b; below
+    return(a + b);
+  }
 
-	}
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int a;
-		a = in.nextInt();
-		int b;
-		b = in.nextInt();
-		int sum;
-		sum = solveMeFirst(a, b);
-		System.out.println(sum);
-	}
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int a;
+    a = in.nextInt();
+    int b;
+    b = in.nextInt();
+    int sum;
+    sum = solveMeFirst(a, b);
+    System.out.println(sum);
+  }
 }
