a766654c11fc16de3128bdab0854f90c58dfa37b
Theron E. Weimer, Jr <tad98696@gmail.com>
2019-04-30 17:12:47 -0400

    Add Java solution for "A Very Big Sum".
    Add source code solution validated by "Testcase 0".

--- algorithms/warmup/AVeryBigSum/AVeryBigSumJava/AVeryBigSumJava.java
+++ algorithms/warmup/AVeryBigSum/AVeryBigSumJava/AVeryBigSumJava.java
@@ -6,12 +6,15 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class AVeryBigSumJava {
 
   // Complete the aVeryBigSum function below.
   static long aVeryBigSum(long[] ar) {
-
-
+    long sum = 0;
+    for(int i = 0; i < ar.length; ++i) {
+      sum += ar[i];
+    }
+    return(sum);
   }
 
   private static final Scanner scanner = new Scanner(System.in);
